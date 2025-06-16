9edef4528b2f6eff3b5792564d4e3113317dfdfe
None <None>
2025-06-16 01:16:15 -0700

    change test topo to triangle

--- testBGP/testFRR.py
+++ testBGP/testFRR.py
@@ -29,9 +29,10 @@ class Router(Switch):
         self.cmd("sudo sysctl -w net.ipv4.ip_forward=1")
         self.waitOutput()
         sleep(0.2)
-        self.cmd("/usr/lib/frr/zebra -f ./test_conf/%s_zebra.conf -d -i /tmp/%s_zebra.pid > ./test_log/%s_zebra-stdout.log 2>&1" % (r, r, r), shell=True)
+        self.cmd("/usr/lib/frr/zebra -f ./test_conf/%s.conf -d -i /tmp/%s_zebra.pid > ./test_log/%s_zebra-stdout.log 2>&1" % (r, r, r), shell=True)
         self.waitOutput()
-        self.cmd("/usr/lib/frr/bgpd -f ./test_conf/%s_bgpd.conf -d -i /tmp/%s_bgpd.pid > ./test_log/%s_bgpd-stdout.log 2>&1" % (r, r, r), shell=True)
+        self.cmd("/usr/lib/frr/bgpd -f ./test_conf/%s.conf -d -i /tmp/%s_bgpd.pid > ./test_log/%s_bgpd-stdout.log 2>&1" % (r, r, r), shell=True)
+        # zebra and bgpd can share the same config file, or have their own config files. Both will do.
         self.waitOutput()
         self.cmd("ifconfig lo up")
         self.waitOutput()
@@ -44,19 +45,26 @@ class MyTopo( Topo ):
     def build(self):
         h1 = self.addHost('h1', ip = "10.0.1.1/24", defaultRoute = "via 10.0.1.254")
         h2 = self.addHost('h2', ip = "10.0.2.1/24", defaultRoute = "via 10.0.2.254")
+        h3 = self.addHost('h3', ip = "10.0.3.1/24", defaultRoute = "via 10.0.3.254")
 
         r1 = self.addSwitch('r1')
         r2 = self.addSwitch('r2')
+        r3 = self.addSwitch('r3')
 
         self.addLink(r1, r2, intfName1="r1-eth2", intfName2="r2-eth2")
+        self.addLink(r1, r3, intfName1="r1-eth3", intfName2="r3-eth2")
+        self.addLink(r2, r3, intfName1="r2-eth3", intfName2="r3-eth3")
+
         self.addLink(h1, r1, intfName2="r1-eth1")
         self.addLink(h2, r2, intfName2="r2-eth1")
+        self.addLink(h3, r3, intfName2="r3-eth1")
 
         # If no intfName was specified, Mininet will by default assign names to links in order.
         # For example in this case, r1's interface would have been: 
         # r1-eth1 : r2
-        # r1-eth2 : h1
-        # Since the link to r2 was established before the link to h1.
+        # r1-eth2 : r3
+        # r1-eth3 : h1
+        # Since the link to r2 was established first, then the link to r3, and finally h1.
         # But with intfName specified, we can assign names to our desired links, not worrying about order.
 
 
